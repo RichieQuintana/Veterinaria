1023ebe9b5eaf691d5075fc5ea07dc4869ff9786
Ricardo Quintana <ricardo.quintana.maldonado@udla.edu.ec>
2023-10-29 18:57:42 -0500

    Creamos las clases en Models: Citas, Cliente, Mascotas e historial de citas

---
+++ Veterinaria/Data/VeterinariaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Veterinaria.Models;
+
+namespace Veterinaria.Data
+{
+    public class VeterinariaContext : DbContext
+    {
+        public VeterinariaContext (DbContextOptions<VeterinariaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Veterinaria.Models.Cliente> Cliente { get; set; } = default!;
+    }
+}
--- Veterinaria/Program.cs
+++ Veterinaria/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Veterinaria.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<VeterinariaContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("VeterinariaContext") ?? throw new InvalidOperationException("Connection string 'VeterinariaContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
